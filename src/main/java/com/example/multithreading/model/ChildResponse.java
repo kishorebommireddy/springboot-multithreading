2daf554c3d73231e0e33ffa7d034104ff7be975d
kishore <kishore.reddy@yahoo.co.in>
2026-03-10 21:47:28 +0800

    First Commit - Child Response

---
+++ src/main/java/com/example/multithreading/model/ChildResponse.java
@@ -0,0 +1,10 @@
+package src.main.java.com.example.multithreading.model;
+
+
+public class ChildResponse {
+    private String serviceName;
+    private String message;
+    private long processingTimeMs;
+    private String status;
+    private int port;
+}
